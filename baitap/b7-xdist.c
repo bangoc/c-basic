54c46dc1f27129c4f70cdc4477a304ad668d084c
Nguyen Ba Ngoc <nguyen_bn@hotmail.com>
2021-11-09 14:32:48 +0700

    Cập nhật kiểm thử xdist-bài 7

--- baitap/b7-xdist.c
+++ baitap/b7-xdist.c
@@ -20,25 +20,25 @@ int t1() {
   bn_connect2(n3, left, n2, top);
   bn_connect2(n2, right, n1, top);
   bn_connect2(n6, left, n4, top);
-  bn_connect2(n1, right, n8, top);
   bn_connect2(n6, right, n7, top);
-  bn_connect2(n7, left, n8, top);
+  bn_connect2(n4, right, n8, top);
   bn_connect2(n7, right, n9, top);
-  bn_connect2(n8, right, n10, top);
+  bn_connect2(n9, right, n10, top);
   ASSERT(bn_xdist(t, n5) == 0, "xdist n5 == 0");
   ASSERT(bn_xdist(t, n3) == -1, "xdist n3");
   ASSERT(bn_xdist(t, n6) == 1, "xdist n6");
   /*
-          n5
-      n3         n6
-   n2         n4     n7
-      n1          n8     n9
-                            n10
+             n5
+      n3           n6
+   n2          n4        n7
+      n1           n8        n9
+                                 n10
   */
   ASSERT(bn_xdist(t, n2) == -2, "n2: -2");
   ASSERT(bn_xdist(t, n1) == -1, "n1: -1");
   ASSERT(bn_xdist(t, n4) == 0, "n4: 0");
   ASSERT(bn_xdist(t, n8) == 1, "n8: 1");
+  ASSERT(bn_xdist(t, n9) == 3, "n9: 3");
   ASSERT(bn_xdist(t, n10) == 4, "n10: 4");
   return 0;
 }
